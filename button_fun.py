fabaa21ea39b04fbf4139c72a2ea745bc42821c7
anusika <anusikanijher@gmail.com>
2021-11-06 11:19:33 -0700

    testing button

--- button_fun.py
+++ button_fun.py
@@ -4,11 +4,18 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(25, GPIO.IN)
+GPIO.add_event_detect(25, GPIO.RISING, callback=change_step, bouncetime=300)  
+
+step = 1
+channel = 25;
+count = 0;
+
+def change_step (channel=channel)
+	if (step == 1) step = 2
+	else step = 1
 
 
 while True:
-	if GPIO.input(25):
-		print("Pin 22 is HIGH")
-	else:
-		print("Pin 22 is LOW")
-	time.sleep(2)
+	count = count + step
+	print(count + "\n")
+	time.sleep(1)
