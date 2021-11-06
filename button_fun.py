6c4d3bc7d6f937e87bf11bb2df4c0aa3dfb8c8a6
anusika <anusikanijher@gmail.com>
2021-11-06 11:22:39 -0700

    testing button

--- button_fun.py
+++ button_fun.py
@@ -4,15 +4,18 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(25, GPIO.IN)
+
+def change_step (channel=channel):
+	if (step == 1): step = 2
+	else: step = 1
+
+
 GPIO.add_event_detect(25, GPIO.RISING, callback=change_step, bouncetime=300)  
 
 step = 1
 channel = 25
 count = 0
 
-def change_step (channel=channel):
-	if (step == 1): step = 2
-	else: step = 1
 
 
 while True:
