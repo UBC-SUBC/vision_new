2f9daf25f3c2dfb309043cbb1a3ab1ce3cd28d72
wilson tu <s31302@gmail.com>
2022-05-06 11:27:04 -0700

    ***Daq Serial Connection Recovering***
    arduinoConnector.py:
    -added some conditions which allow the serial connection to recover if it has failed
    -TODO: causes a small lagspike when recovering, consider running on background thread

--- pyqtTest/arduinoConnector.py
+++ pyqtTest/arduinoConnector.py
@@ -12,24 +12,46 @@ class ArduinoConnector:
              'speed': "-99999", 'depth': "-99999",'battery':False}
         
          #serial setup
-         self.ser=serial.Serial(
-         port= ArduinoVar.serialPiPort,
-         baudrate = 115200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=1
-         )
-         self.ser.flush()
+         try:
+             self.ser=serial.Serial(
+             port= ArduinoVar.serialPiPort,
+             baudrate = 115200,
+                 parity=serial.PARITY_NONE,
+                 stopbits=serial.STOPBITS_ONE,
+                 bytesize=serial.EIGHTBITS,
+                 timeout=1
+             )
+             self.ser.flush()
+         except:
+             self.ser = None
+             print('No DAQ board detected')   
     
      # Read serial data from the daq system. All values are passed as a JSON
      def readJsonFromArduino(self):
+         # we try to create a new serial object if one is not currently available in order to recover our connection
+         if self.ser is None:
+             try:
+                 self.ser=serial.Serial(
+                 port= ArduinoVar.serialPiPort,
+                 baudrate = 115200,
+                     parity=serial.PARITY_NONE,
+                     stopbits=serial.STOPBITS_ONE,
+                     bytesize=serial.EIGHTBITS,
+                     timeout=1
+                 )
+                 self.ser.flush()
+             except:
+                 self.ser = None
+                 print('No DAQ board detected try reconnecting')   
          try:             
              line = self.ser.readline().decode('utf-8')
              line.replace('\n', '')
              line.replace('\r', '')
              DataToDisplay = json.loads(line)
          except:
+             if self.ser is not None:
+                self.ser.close()
+                self.ser = None
              DataToDisplay = self.ErrorData
          print(DataToDisplay)
          return DataToDisplay
