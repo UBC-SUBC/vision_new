520f3a319d682eb86e80b1192f856f2e052ee4b2
tu1026 <s31302@gmail.com>
2022-05-14 14:22:20 -0700

    delete old tests

--- Tests/testSSH.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import pyautogui
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
-im1 = pyautogui.screenshot()
-im1.save("/home/pi/Desktop/PyScreen" + current_time + ".png")
