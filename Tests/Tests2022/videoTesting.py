ded10a93b850f757613faebbf0f1860119415f18
tu1026 <s31302@gmail.com>
2022-05-14 14:22:29 -0700

    adding tests for pyqt 2022

--- Tests/Tests2022/videoTesting.py
+++ Tests/Tests2022/videoTesting.py
@@ -16,7 +16,8 @@ cap= cv2.VideoCapture(0)
 width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-writer= cv2.VideoWriter(os.path.join(output_dir, 'test_videos.mp4'), cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
+# writer= cv2.VideoWriter(os.path.join(output_dir, 'test_videos.mp4'), cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
+writer= cv2.VideoWriter(os.path.join(output_dir, 'test_videos.avi'), cv2.VideoWriter_fourcc('M','J','P','G'), 20, (int(cap.get(3)),int(cap.get(4))))
 
 curr_time = datetime.datetime.now()
 
