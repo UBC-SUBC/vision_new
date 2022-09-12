fd7c84ef6444e1f342f1f4621da1715ffd9e213c
artyblouin <artyblouin@gmail.com>
2022-09-11 19:11:20 -0700

    Took the function for the LED detection pipeline and put it in an object that we can instantiate in the main code somewhere. Discussion needs to be had with Wilson about where the best place for that might be. Wrote a function for loading a new image into the class so that the pipeline can the be run on the new frame. Added a function for displaying the latest image and its associated result after running through the detection pipeline. Wrote a constructor function in which we can load upper and lower bounds for LED detection. Could be good to load this from a config when integrated with the main code.

--- circle_detect.py
+++ circle_detect.py
@@ -2,38 +2,59 @@
 import cv2
 import numpy as np  
 
-# This function takes a BGR image as its inputs
-# The function returns the location of the detected LEDs and a linear fit of their locations (slope and intercept)
-# TODO: Improve this function by testing it in various lighting situations, this will be the core of our detection pipeline
-def find_LEDs(image = cv2.Mat, lower_bound = int, upper_bound = int):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.medianBlur(gray, 5)
-    
-    # Create the mask
-    mask = cv2.inRange(img_blur, lower_bound, upper_bound)
-    mask = cv2.bitwise_not(mask)
 
-    detector = cv2.SimpleBlobDetector_create()
+class ComputerVisionModule():
+    lower_bound = int
+    upper_bound = int
 
-    # Detect blobs.
-    keypoints = detector.detect(mask)
+    latest_image = cv2.Mat
 
-    line = np.polyfit([x.pt[0] for x in keypoints], [y.pt[1] for y in keypoints], 1)
+    keypoints = [cv2.KeyPoint]
+    line = [float]
 
-    return keypoints, line
+    def __init__(self, lower_bound, upper_bound):
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
 
-# # Example use case for the find_LEDs function (uncomment the code to see the function in action): 
-# image  = cv2.imread('org_image.jpg')
 
-# keypoints, line = find_LEDs(image, 200, 255)
+    def load_new_image(self, image = cv2.Mat):
+        self.latest_image = image
 
-# # Draw detected blobs as red circles.
-# # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-# im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # This function takes a BGR image as its inputs
+    # The function returns the location of the detected LEDs and a linear fit of their locations (slope and intercept)
+    # TODO: Improve this function by testing it in various lighting situations, this will be the core of our detection pipeline
+    def find_LEDs(self):
+        gray = cv2.cvtColor(self.latest_image, cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.medianBlur(gray, 5)
+        
+        # Create the mask
+        mask = cv2.inRange(img_blur, self.lower_bound, self.upper_bound)
+        mask = cv2.bitwise_not(mask)
 
-# im_with_line = cv2.line(im_with_keypoints, (int(keypoints[0].pt[0]), int(keypoints[0].pt[1])),  (int(keypoints[len(keypoints) - 1].pt[0]), int(keypoints[len(keypoints) - 1].pt[1])),  (0, 0, 255), 3)
+        detector = cv2.SimpleBlobDetector_create()
 
-# # Show keypoints
-# cv2.imshow("Keypoints", im_with_line)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+        # Detect blobs.
+        self.keypoints = detector.detect(mask)
+
+        self.line = np.polyfit([x.pt[0] for x in self.keypoints], [y.pt[1] for y in self.keypoints], 1)
+
+    def display_image(self):
+
+        # Draw detected blobs as red circles.
+        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+        im_with_keypoints = cv2.drawKeypoints(self.latest_image, self.keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        im_with_line = cv2.line(im_with_keypoints, (int(self.keypoints[0].pt[0]), int(self.keypoints[0].pt[1])),  (int(self.keypoints[len(self.keypoints) - 1].pt[0]), int(self.keypoints[len(self.keypoints) - 1].pt[1])),  (0, 0, 255), 3)
+
+        # Show keypoints
+        cv2.imshow("Keypoints", im_with_line)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+# The following code is commented out so that it can be used when debugging, it creates an instance of the ComputerVisionModule class 
+# and runs a single instance of the detection pipeline
+
+# img_processor = ComputerVisionModule(lower_bound=200, upper_bound=255)
+# img_processor.load_new_image(cv2.imread('org_image.jpg'))
+# img_processor.find_LEDs()
+# img_processor.display_image()
