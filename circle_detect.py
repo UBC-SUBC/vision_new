28bd2ce438533dfcc4315cc964c6053ced18b971
artyblouin <artyblouin@gmail.com>
2022-09-13 00:17:49 -0700

    Modified the computervisionmdoule class so that it would read parameters from a specified JSON file when instantiated. Added an example JSON file with the parameters for the computervisionmodule class Add a whole bunch of pictures of lights that can be used for validating the pipeline

--- circle_detect.py
+++ circle_detect.py
@@ -1,7 +1,17 @@
 # import the opencv library
 import cv2
+import json
 import numpy as np  
 
+
+'''
+
+This file contains a class with functions for the LED detection pipeline for SUBC's vision system. 
+It is currently configured to read in thresholds from a JSON file and can hold one image within itself at any given time.
+TODO: It would be a good idea to implement some sort of scoring algorithm to grade our detection performance. This could
+exist as a function in the class and could be re-run with different parameters to track improvements in detection over time.
+
+'''
 
 class ComputerVisionModule():
     lower_bound = int
@@ -12,11 +22,31 @@ class ComputerVisionModule():
     keypoints = [cv2.KeyPoint]
     line = [float]
 
-    def __init__(self, lower_bound, upper_bound):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
+
+    # Constructor for the computer vision module of the code. 
+    # Currently set up to take its parameters from a specified JSON file for simple testing of different configurations
+    def __init__(self, json_path: str):
+
+        # Load Json file from specified path
+        params_json = json.load(open(json_path, 'r'))
 
 
+        # Define thresholds and parameters relevant to our computer vision pipeline for detecting LEDs
+        self.lower_bound = int(params_json['LowerBound'])
+        self.upper_bound = int(params_json['UpperBound'])
+
+        self.params = cv2.SimpleBlobDetector_Params()
+
+        self.params.minDistBetweenBlobs = int(params_json['MinDistBetweenBlobs'])
+        self.params.filterByInertia = (params_json['FilterByInertia'] == 'True')
+        self.params.filterByConvexity = (params_json['FilterByConvexity'] == 'True')
+        self.params.filterByColor = (params_json['FilterByColor'] == 'True')
+        self.params.filterByCircularity = (params_json['FilterByCircularity'] == 'True')
+        self.params.filterByArea = (params_json['FilterByArea'] == 'True')
+        self.params.minArea = float(params_json['MinArea'])
+        self.params.maxArea = float(params_json['MaxArea'])
+
+    # Loads a new image into the object and over writes the previous one
     def load_new_image(self, image = cv2.Mat):
         self.latest_image = image
 
@@ -26,14 +56,13 @@ class ComputerVisionModule():
     def find_LEDs(self):
         gray = cv2.cvtColor(self.latest_image, cv2.COLOR_BGR2GRAY)
         img_blur = cv2.medianBlur(gray, 5)
-        
-        # Create the mask
+
+        # Create a mask of only the colors within the specified gray scale thresholds
         mask = cv2.inRange(img_blur, self.lower_bound, self.upper_bound)
         mask = cv2.bitwise_not(mask)
 
-        detector = cv2.SimpleBlobDetector_create()
-
-        # Detect blobs.
+        detector = cv2.SimpleBlobDetector_create(self.params) 
+        
         self.keypoints = detector.detect(mask)
 
         self.line = np.polyfit([x.pt[0] for x in self.keypoints], [y.pt[1] for y in self.keypoints], 1)
@@ -53,8 +82,7 @@ class ComputerVisionModule():
 
 # The following code is commented out so that it can be used when debugging, it creates an instance of the ComputerVisionModule class 
 # and runs a single instance of the detection pipeline
-
-# img_processor = ComputerVisionModule(lower_bound=200, upper_bound=255)
-# img_processor.load_new_image(cv2.imread('org_image.jpg'))
+# img_processor = ComputerVisionModule(json_path='./JSON/ComputerVision.json')
+# img_processor.load_new_image(cv2.imread('./images/IMG_20220912_210543108.jpg'))
 # img_processor.find_LEDs()
 # img_processor.display_image()
