58bbcf3292867816b22a1566c34285d6f1085ac4
Arthur Blouin <72779633+artyblouin@users.noreply.github.com>
2023-01-22 20:27:10 -0800

    Update circle_detect.py
    Hough line implementation for detecting the strip of LEDs at the bottom of the pool
    
    Signed-off-by: Arthur Blouin <72779633+artyblouin@users.noreply.github.com>

--- circle_detect.py
+++ circle_detect.py
@@ -2,16 +2,43 @@
 import cv2
 import json
 import numpy as np  
-
+import math
 
 '''
-
 This file contains a class with functions for the LED detection pipeline for SUBC's vision system. 
 It is currently configured to read in thresholds from a JSON file and can hold one image within itself at any given time.
 TODO: It would be a good idea to implement some sort of scoring algorithm to grade our detection performance. This could
 exist as a function in the class and could be re-run with different parameters to track improvements in detection over time.
-
 '''
+
+class PIDControllerModule():
+    def __init__(self, setpoint, kp, ki, kd, fps):
+
+        self.setpoint = setpoint
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        
+        self.fps = fps
+
+        self.I_error = 0
+        self.P_error = 0
+        self.D_error = 0
+
+    def receive_new_process_variables(self, pv):
+        last_error = self.P_error
+        
+        self.P_error = self.setpoint - pv
+        self.I_error += self.P_error * (1 / self.fps)
+
+        # Assumption is made that we know the acquiring frame rate, we could also use timestamps with our data
+        self.D_error = (self.P_error - last_error) / (1 / self.fps)
+
+        self.PID_result = (self.kp * self.P_error) + (self.ki * self.I_error) + (self.kd * self.D_error)
+
+        print(self.PID_result)
+
+
 
 class ComputerVisionModule():
     lower_bound = int
@@ -55,34 +82,31 @@ class ComputerVisionModule():
     # TODO: Improve this function by testing it in various lighting situations, this will be the core of our detection pipeline
     def find_LEDs(self):
         gray = cv2.cvtColor(self.latest_image, cv2.COLOR_BGR2GRAY)
-        img_blur = cv2.medianBlur(gray, 5)
+        img_blur = cv2.medianBlur(gray, 3)
 
         # Create a mask of only the colors within the specified gray scale thresholds
         mask = cv2.inRange(img_blur, self.lower_bound, self.upper_bound)
-        mask = cv2.bitwise_not(mask)
 
-        detector = cv2.SimpleBlobDetector_create(self.params) 
-        
-        self.keypoints = detector.detect(mask)
+        lines = cv2.HoughLinesP(mask, 1, np.pi/180, threshold = 100, minLineLength=200, maxLineGap = 3)
 
-        self.line = np.polyfit([x.pt[0] for x in self.keypoints], [y.pt[1] for y in self.keypoints], 1)
+        # The below for loop runs till r and theta values
+        # are in the range of the 2d array
 
-    def display_image(self):
+        if lines is not None:
+            theta_list = []
 
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv2.drawKeypoints(self.latest_image, self.keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            for points in lines:
+                # Extracted points nested in the list
+                x1,y1,x2,y2=points[0]
 
-        im_with_line = cv2.line(im_with_keypoints, (int(self.keypoints[0].pt[0]), int(self.keypoints[0].pt[1])),  (int(self.keypoints[len(self.keypoints) - 1].pt[0]), int(self.keypoints[len(self.keypoints) - 1].pt[1])),  (0, 0, 255), 3)
+                x_distance = x2 - x1
+                y_distance = y2 - y1
 
-        # Show keypoints
-        cv2.imshow("Keypoints", im_with_line)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+                theta = math.atan(x_distance/y_distance)
 
-# The following code is commented out so that it can be used when debugging, it creates an instance of the ComputerVisionModule class 
-# and runs a single instance of the detection pipeline
-# img_processor = ComputerVisionModule(json_path='./JSON/ComputerVision.json')
-# img_processor.load_new_image(cv2.imread('./images/IMG_20220912_210543108.jpg'))
-# img_processor.find_LEDs()
-# img_processor.display_image()
+                theta_list.append(theta)
+                # Draw the lines joing the points
+                # On the original image
+                cv2.line(self.latest_image,(x1,y1),(x2,y2),(0,0,255 ),2)
+
+            self.theta_avg = sum(theta_list)/len(theta_list)
