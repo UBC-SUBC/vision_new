38af17274dfe6351d9831e47e58702cba946a8a7
LxsaChn <62408538+LxsaChn@users.noreply.github.com>
2023-02-18 12:05:21 -0800

    Update edge.py
    Make it b&w to add clarity
    
    Signed-off-by: LxsaChn <62408538+LxsaChn@users.noreply.github.com>

--- edge_detection/edge.py
+++ edge_detection/edge.py
@@ -3,36 +3,33 @@ import cv2
 import numpy as np  
 
 def canny(frame):
-    # Convert to graycsale
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Blur the image for better edge detection
-    img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-    # Canny Edge Detection
-    sigma = np.std(img_blur)
-    mean = np.mean(img_blur)
-    lower = int(max(0, (mean - sigma)))
-    upper = int(min(255, (mean + sigma)))
 
-    edges = cv2.Canny(image=img_blur, threshold1=lower, threshold2=upper) # Canny Edge Detection
-    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)  #convert canny image to bgr
-    #edges *= np.array((0,0,1),np.uint8)
-    #cv2.line(edges, (50, 100), (150, 100), (255,255,255), 1)
-    #add_weight = cv2.addWeighted( frame, 0.5, edges, 0.5, 0.0) # blend src image with canny image
-    new_image = cv2.add(frame, edges) # add src image with canny image
+    # Blur the image for better edge detection
+
+    img_blur = cv2.GaussianBlur(frame, (3,3), 0)       
+    # Convert to graycsale
+    img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    #threshold grayscale image to extract glare
+    mask1 = cv2.threshold(img_gray, 220, 255, cv2.THRESH_BINARY)[1]
+    #inpainting
+    result_1 = cv2.inpaint(img_gray, mask1, 21, cv2.INPAINT_TELEA)
+    
+    max_value = np.max(result_1)
+    threshold_value = int(max_value * 0.8)
+    mask = cv2.threshold(result_1, threshold_value, 255, cv2.THRESH_BINARY)[1]
+    result_2 = cv2.bitwise_and(result_1, result_1, mask=mask)
+
     # Display Canny Edge Detection Image
-    cv2.imshow('Canny Edge Detection',edges)
+    cv2.imshow('Canny Edge Detection',result_2)
 
 # define a video capture object
-vid = cv2.VideoCapture("20181212_123100.MOV")
+vid = cv2.VideoCapture("videoplayback.mp4")
   
 while(True):
       
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
 
     canny(frame)
     
