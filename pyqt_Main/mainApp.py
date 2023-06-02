cfb3d072717ad387c4ab156c8b80922bae56f8cc
Wilson Tu <s31302@gmail.com>
2023-06-02 14:03:25 -0700

    sizing

--- pyqt_Main/mainApp.py
+++ pyqt_Main/mainApp.py
@@ -433,7 +433,7 @@ class videoOverlayStatic(QLabel):
         painter.drawPixmap(contextPerserver.width*0.08,self.bot_height, QPixmap.fromImage(self.beam_img))
 
         if not self.imu_status:
-            self.imu_image = self.imu_image.scaled(contextPerserver.width*0.05, self.top_height*2)
+            self.imu_image = self.imu_image.scaled(contextPerserver.width*0.05, self.top_height/2)
             painter.drawPixmap(contextPerserver.width*0.14,self.bot_height, QPixmap.fromImage(self.imu_image))
     
     def paintEvent(self, event):
