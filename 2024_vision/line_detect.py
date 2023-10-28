15da2dc7cf91e2d2cee29e720abab5ae65d8e48c
ericfb04 <ericfb04@outlook.com>
2023-10-28 15:51:41 -0700

    Need to work on either Canny or Laplacian edge detection on image to get rid of extra noise on processed image

--- 2024_vision/line_detect.py
+++ 2024_vision/line_detect.py
@@ -16,8 +16,10 @@ class LineDetect:
     def detect(self, frame):
         # convert to greyscale
         img_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.GaussianBlur(img_grey, (3, 3), 0)
         # apply Otsu thresholding to highlight black as white
-        ret, thresh1 = cv2.threshold(img_grey,81, 255, cv2.THRESH_BINARY_INV)
+        ret, thresh1 = cv2.threshold(img_blur,81, 255, cv2.THRESH_BINARY_INV)
+        # apply Canny for edge detection
         cv2.imshow('test', thresh1)
         cv2.waitKey(0)
 
